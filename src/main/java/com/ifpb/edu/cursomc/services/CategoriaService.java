d83f1a12a7b116f64950eb5ad6868f8e77470dcb
romuloqaz <romulo.pereiracz@gmail.com>
2020-02-18 09:13:28 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/ifpb/edu/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.ifpb.edu.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifpb.edu.cursomc.domain.Categoria;
+import com.ifpb.edu.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired //injeção de dependencia spring
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}
