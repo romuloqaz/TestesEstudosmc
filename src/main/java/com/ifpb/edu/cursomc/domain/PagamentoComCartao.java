b81d65b1beb7205b2679570457adad844c8bc339
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-03 18:29:44 -0300

    inserindo Pedido

--- src/main/java/com/ifpb/edu/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/ifpb/edu/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.ifpb.edu.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.ifpb.edu.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
