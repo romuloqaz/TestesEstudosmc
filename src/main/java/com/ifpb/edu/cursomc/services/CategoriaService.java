f803e98af2098e7a8a2f6485811f7069019d57e7
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-03 10:30:55 -0300

    validação sintatica com bean validation

--- src/main/java/com/ifpb/edu/cursomc/services/CategoriaService.java
+++ src/main/java/com/ifpb/edu/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ifpb.edu.cursomc.domain.Categoria;
+import com.ifpb.edu.cursomc.dto.CategoriaDTO;
 import com.ifpb.edu.cursomc.repositories.CategoriaRepository;
 import com.ifpb.edu.cursomc.services.exceptions.DataIntegretyException;
 import com.ifpb.edu.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,9 @@ public class CategoriaService {
 		return repo.findAll(pageRequest);
 	}
 	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
+	}
 	
 	
 }
