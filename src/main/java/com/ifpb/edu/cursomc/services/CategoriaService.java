32cc4f6fe33d40f7bb9501dedf1bfeeedd0675b5
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-02 22:22:20 -0300

    Deletando uma categoria com delete

--- src/main/java/com/ifpb/edu/cursomc/services/CategoriaService.java
+++ src/main/java/com/ifpb/edu/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ifpb.edu.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ifpb.edu.cursomc.domain.Categoria;
 import com.ifpb.edu.cursomc.repositories.CategoriaRepository;
+import com.ifpb.edu.cursomc.services.exceptions.DataIntegretyException;
 import com.ifpb.edu.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update (Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
---
+++ src/main/java/com/ifpb/edu/cursomc/services/exceptions/DataIntegretyException.java
@@ -0,0 +1,13 @@
+package com.ifpb.edu.cursomc.services.exceptions;
+
+public class DataIntegretyException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegretyException(String msg) {
+		super(msg);
+	}
+	public DataIntegretyException (String msg, Throwable cause) {
+		super(msg,cause);
+	}
+}
