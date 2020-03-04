1531dcc642276e57c879c3ab4fd219dfb3138f6c
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-04 13:10:07 -0300

    criando profile de desenvolvimento

--- src/main/java/com/ifpb/edu/cursomc/config/DevConfig.java
+++ src/main/java/com/ifpb/edu/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.ifpb.edu.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,18 +11,23 @@ import org.springframework.context.annotation.Profile;
 import com.ifpb.edu.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
 		dbService.instantiateTestDatabase();
 		return true;
 	}
-	
-
-	
 }
