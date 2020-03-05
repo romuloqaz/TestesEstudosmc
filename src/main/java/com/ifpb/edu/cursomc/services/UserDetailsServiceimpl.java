8c0fb41b46711a6f04ee60d14a2e302908b14d0d
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-05 17:53:30 -0300

    Implementando autenticação e geranco token jwt

---
+++ src/main/java/com/ifpb/edu/cursomc/services/UserDetailsServiceimpl.java
@@ -0,0 +1,28 @@
+package com.ifpb.edu.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ifpb.edu.cursomc.domain.Cliente;
+import com.ifpb.edu.cursomc.repositories.ClienteRepository;
+import com.ifpb.edu.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceimpl implements UserDetailsService{
+
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
