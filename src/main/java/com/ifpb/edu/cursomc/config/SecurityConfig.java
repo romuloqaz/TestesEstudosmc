c31bd8c7dd1e4fa407bd6d0b3f54aba822099771
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-05 18:29:01 -0300

    Implementando autorizacao

--- src/main/java/com/ifpb/edu/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ifpb/edu/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ifpb.edu.cursomc.security.JWTAuthenticationFilter;
+import com.ifpb.edu.cursomc.security.JWTAuthorizationFilter;
 import com.ifpb.edu.cursomc.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
