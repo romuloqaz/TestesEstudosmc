25a73b15bc4802c97f0772f04748f5d09b5c054f
romuloqaz <romulo.pereiracz@gmail.com>
2020-03-06 11:30:59 -0300

    esqueci a senha

--- src/main/java/com/ifpb/edu/cursomc/resources/AuthResource.java
+++ src/main/java/com/ifpb/edu/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.ifpb.edu.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ifpb.edu.cursomc.dto.EmailDTO;
 import com.ifpb.edu.cursomc.security.JWTUtil;
 import com.ifpb.edu.cursomc.security.UserSS;
+import com.ifpb.edu.cursomc.services.AuthService;
 import com.ifpb.edu.cursomc.services.UserService;
 
 @RestController
@@ -18,12 +22,21 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());//pega usuario logado
 		response.addHeader("Authorization", "Bearer " + token);//adiciona novo token na resposta da requisiçao
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
